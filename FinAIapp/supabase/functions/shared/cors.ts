9604c981eb40a1a83a4feaaf1cfc5e7944f4b524
Santiago Alexander Rubiano Sanchez <thiagosnchz.02@gamil.com>
2025-04-11 17:27:58 +0200

    Creacion de funciones complejas de la pagina settings, creacion de edge functions, configuracion de docker y deno y archivos de configuracion de deno. v4

--- FinAIapp/supabase/functions/shared/cors.ts
+++ FinAIapp/supabase/functions/shared/cors.ts
@@ -5,5 +5,6 @@ export const corsHeaders = {
     // Para desarrollo local puedes usar '*' o 'http://127.0.0.1:xxxx' (tu puerto de Live Server)
     // Para producción, cámbialo a tu dominio: 'https://finai.es'
     'Access-Control-Allow-Origin': '*',
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type', // Headers necesarios para Supabase
   }
